d9ff3f45e44977e70017382e89f177b9f1588fc6
ALRW <awerner1@googlemail.com>
2020-07-24 14:57:06 +0100

    [AW] taking the react example and rewriting in vanilla js

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-const element = React.createElement(
-  'h1',
-  { title: 'foo' },
-  "Hello"
-)
+// const element = <h1 title="foo">Hello</h1>
+const element = {
+  type: 'h1',
+  props: {
+    title: 'foo',
+    children: 'Hello'
+  },
+}
 
 const container = document.getElementById('root')
 
-ReactDOM.render(element, container)
+// ReactDOM.render(element, container)
+const node = document.createElement(element.type)
+node['title'] = element.props.title
+const text = document.createTextNode('')
+text['nodeValue'] = element.props.children
+node.appendChild(text)
+container.appendChild(node)
